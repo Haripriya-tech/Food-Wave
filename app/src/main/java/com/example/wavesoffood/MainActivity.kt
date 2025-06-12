54b916a92de41ff5c64e6ddac335aa0a5404d16b
Rakshita B R <rakshita.br0505@gmail.com>
2025-06-12 12:26:31 +0530

    Remove unused import

--- app/src/main/java/com/example/wavesoffood/MainActivity.kt
+++ app/src/main/java/com/example/wavesoffood/MainActivity.kt
@@ -1,12 +1,8 @@
 package com.example.wavesoffood
 
 
-import android.annotation.SuppressLint
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
